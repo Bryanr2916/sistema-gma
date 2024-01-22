41a16dd98bc4de06dee8c960a1e28178c12ce0f1
Bryan Rodríguez <bryanr2916@gmail.com>
2024-01-21 23:55:49 -0600

    nav principal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
@@ -32,7 +32,8 @@ import { UsuarioModule } from './usuario/usuario.module';
     NormativasModule,
     AreaLegalModule,
     BuscarModule,
-    UsuarioModule
+    UsuarioModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
