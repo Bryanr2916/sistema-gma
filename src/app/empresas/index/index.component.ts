a01ffd601c603a2d23bd7186dac1ef60ba6ae136
Bryan Rodríguez <bryanr2916@gmail.com>
2024-01-23 14:13:10 -0600

    tabla generica

--- src/app/empresas/index/index.component.ts
+++ src/app/empresas/index/index.component.ts
@@ -6,6 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./index.component.scss']
 })
 export class IndexComponent implements OnInit {
+  acciones= ["Usar como |", "Editar |", "Borrar |", "Duplicar"]
+  ths = ["#","Empresa", "Administrador", "Correo Electrónico", "Acciones"];
+  trs = [
+    { nombre: "empresa 1", admin: "admin 1", correo: "correo1@empresa.com"},
+    { nombre: "empresa 2", admin: "admin 2", correo: "correo2@empresa.com"},
+    { nombre: "empresa 3", admin: "admin 3", correo: "correo3@empresa.com"},
+  ];
 
   constructor() { }
 
