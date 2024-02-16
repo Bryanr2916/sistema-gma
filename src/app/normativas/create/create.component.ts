f50db6970696422d0b4a57ef800ca19d9f5638e6
Bryan Rodríguez <bryanr2916@gmail.com>
2024-02-16 11:23:47 -0600

    cargar tipos de normativas en select

--- src/app/normativas/create/create.component.ts
+++ src/app/normativas/create/create.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { PaisesService } from 'src/app/core/services/paises.service';
+import { TiposNormativasService } from 'src/app/core/services/tipos-normativas.service';
 
 @Component({
   selector: 'app-create',
@@ -9,12 +10,16 @@ import { PaisesService } from 'src/app/core/services/paises.service';
 })
 export class CreateComponent implements OnInit {
 
+  tipos:any[] = [];
   paises = this.paisesService.paises;
 
-  constructor(private titleService: Title, private paisesService: PaisesService) { }
+  constructor(private titleService: Title, private paisesService: PaisesService, private tiposService:TiposNormativasService) { }
 
   ngOnInit(): void {
     this.titleService.setTitle("GMA Sistema - Normativas");
+    this.tiposService.obtenerTipos().subscribe(datos => {
+      this.tipos = datos;
+    });
   }
 
 }
