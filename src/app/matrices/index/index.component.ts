c0d021dcd8bc31245de0ad7a46e81a3c8819b976
Bryan Rodríguez <bryanr2916@gmail.com>
2024-02-29 11:52:58 -0600

    matrices index

--- src/app/matrices/index/index.component.ts
+++ src/app/matrices/index/index.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
+import { EmpresasService } from 'src/app/core/services/empresas.service';
+import { MatricesService } from 'src/app/core/services/matrices.service';
 
 @Component({
   selector: 'app-index',
@@ -7,11 +9,36 @@ import { Title } from '@angular/platform-browser';
   styleUrls: ['./index.component.scss']
 })
 export class IndexComponent implements OnInit {
+  cargando = true;
+  busqueda = "";
+  empresasTodas:any[] = [];
+  empresasMatrices: any[] = [];
+  empresasFiltradas:any[] = [];
+  matrices:any[] = [];
 
-  constructor(private titleService: Title) { }
+  constructor(
+    private titleService: Title,
+    private empresasService: EmpresasService,
+    private matricesService: MatricesService
+  ) { }
 
   ngOnInit(): void {
     this.titleService.setTitle("GMA Sistema - Matrices");
+    this.empresasService.obtenerEmpresas().subscribe(datos => {
+      this.empresasTodas = datos;
+      this.cargarMatrices();
+    });
+  }
+
+  cargarMatrices() {
+    this.matricesService.obtenerMatrices().subscribe(datos => {
+      this.matrices = datos;
+      this.cargando = false;
+    });
+  }
+
+  nombreEmpresa (id: string) {
+    return this.empresasTodas.find(emp => emp.id === id).nombre
   }
 
 }
