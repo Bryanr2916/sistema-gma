0e92e3608c6c195165e7254a85b3c966e66ee0d5
Bryan Rodríguez <bryanr2916@gmail.com>
2025-01-03 18:53:22 -0600

    empresas: editar

--- src/app/empresas/edit/edit.component.ts
+++ src/app/empresas/edit/edit.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
+import { ActivatedRoute } from '@angular/router';
 import { EmpresasService } from 'src/app/core/services/empresas.service';
 import { PaisesService } from 'src/app/core/services/paises.service';
 import { UsuarioService } from 'src/app/core/services/usuario.service';
@@ -11,13 +12,14 @@ import { UsuarioService } from 'src/app/core/services/usuario.service';
 })
 export class EditComponent implements OnInit {
 
-  tabEmpresa = true;
   displayLogo = "";
   archivoLogo:any = null;
+  archivoNuevo:any = null;
  
   paises = this.paisesService.paises;
   
   empresa = {
+    id: "",
     nombre: "",
     correo: "",
     telefono: "",
@@ -39,28 +41,63 @@ export class EditComponent implements OnInit {
 
   constructor(
     private titleService: Title, private empresasService: EmpresasService,
-    private paisesService: PaisesService, private usuarioService:UsuarioService
+    private paisesService: PaisesService, private usuarioService:UsuarioService,
+    private route: ActivatedRoute
   ) { }
 
   ngOnInit(): void {
     this.titleService.setTitle("GMA Sistema - Empresas");
+    this.route.params.subscribe( params => {
+      this.empresa.id = params["id"];
+    });
+
+    this.obtenerEmpresa();
+  }
+
+  obtenerEmpresa = async () => {
+    const respuesta = await this.empresasService.obtenerEmpresa(this.empresa.id);
+    const datosEmpresa = respuesta.data();
+    console.log(datosEmpresa);
+    if (datosEmpresa) {
+      this.empresa.correo = datosEmpresa['correo'];
+      this.empresa.nombre = datosEmpresa['nombre']; 
+      this.empresa.telefono = datosEmpresa['telefono'];
+      this.empresa.pais = datosEmpresa['pais'];
+      this.empresa.urlLogo = datosEmpresa['urlLogo'];
+      this.empresa.notas = datosEmpresa['notas'];
+
+      this.displayLogo = datosEmpresa['urlLogo'];
+      this.archivoLogo = datosEmpresa['urlLogo']
+    }
   }
 
   editarEmpresa() {
-    console.log("editando...");
+    if (this.formularioEsValido()) {
+      if (this.archivoNuevo) {
+        this.empresasService.subirArchivo(this.archivoNuevo).then(respuesta => {
+        this.empresa.urlLogo = respuesta;
+        this.editarEmpresaFB();
+        })
+      } else {
+        this.editarEmpresaFB();
+      }
+    }
+  }
+
+  editarEmpresaFB() {
+    this.empresasService.editarEmpresa(this.empresa).then(rEmpresa => {
+      console.log(rEmpresa);
+    });
   }
 
   formularioEsValido() {
     return true;
   }
 
-  cambiarTabEmpresa( valor: boolean) {
-    this.tabEmpresa = valor;
-  }
-
   mostrarImagen($event:any) {
     if ($event.target.files) {
       this.archivoLogo = $event.target.files[0];
+      this.archivoNuevo = $event.target.files[0];
       let reader = new FileReader();
       reader.readAsDataURL(this.archivoLogo);
       reader.onload = (e:any) => {
