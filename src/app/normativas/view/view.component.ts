86ba5b840655fd24c5eb586c676b78043d36fb0f
Bryan Rodríguez <bryanr2916@gmail.com>
2025-01-04 09:47:50 -0600

    normativas: ver

--- src/app/normativas/view/view.component.ts
+++ src/app/normativas/view/view.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { ActivatedRoute } from '@angular/router';
+import { NormativaService } from 'src/app/core/services/normativa.service';
+import { PaisesService } from 'src/app/core/services/paises.service';
+import { TiposNormativasService } from 'src/app/core/services/tipos-normativas.service';
 
 @Component({
   selector: 'app-view',
@@ -7,9 +12,52 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ViewComponent implements OnInit {
 
-  constructor() { }
+  normativa = {
+    id: "",
+    urlArchivo: "",
+    titulo: "",
+    tipoId:"",
+    pais:"",
+    numero: 0,
+    modificacion:"",
+    fecha:"",
+    entidad:"",
+    enlace:"",
+    comentarios:"",
+  };
+
+  tipo= {
+    id: "",
+    nombre: ""
+  };
+
+  constructor(
+    private titleService: Title, private tiposService: TiposNormativasService,
+    private normativaService:NormativaService,
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
+    this.titleService.setTitle("GMA Sistema - Normativas");
+    this.route.params.subscribe( params => {
+      this.normativa.id = params["id"];
+    });
+    this.normativaService.obtenerNormativa(this.normativa.id).then(respuesta => {
+      console.log("respuesta: ", respuesta);
+      this.normativa.titulo = respuesta.get("titulo");
+      this.normativa.tipoId = respuesta.get("tipoId");
+      this.tiposService.obtenerTipo(this.normativa.tipoId).then(rTipo => {
+        this.tipo.nombre = rTipo.get("nombre");  
+      });
+      this.normativa.numero = respuesta.get("numero");
+      this.normativa.fecha = respuesta.get("fecha");
+      this.normativa.pais = respuesta.get("pais");
+      this.normativa.modificacion = respuesta.get("modificacion");
+      this.normativa.entidad = respuesta.get("entidad");
+      this.normativa.enlace = respuesta.get("enlace");
+      this.normativa.comentarios = respuesta.get("comentarios");
+      this.normativa.urlArchivo = respuesta.get("urlArchivo");
+    });
   }
 
 }
