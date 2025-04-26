5aed1057e59972abdcc7bfbfa40e2833c286469e
Bryan Rodríguez <bryanr2916@gmail.com>
2025-04-26 06:30:54 -0600

    Usuario: validacion reestablecer contrasena

--- src/app/usuario/reestablecer-contrasena/reestablecer-contrasena.component.ts
+++ src/app/usuario/reestablecer-contrasena/reestablecer-contrasena.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Title } from '@angular/platform-browser';
 import { ToastrService } from 'ngx-toastr';
 import { UsuarioService } from 'src/app/core/services/usuario.service';
@@ -10,20 +11,40 @@ import { UsuarioService } from 'src/app/core/services/usuario.service';
 })
 export class ReestablecerContrasenaComponent implements OnInit {
 
+  formulario: FormGroup = this.fb.group({});
   correo = "";
 
   constructor(
     private titleService: Title,
     private usuarioService: UsuarioService,
-    private toastr: ToastrService
-  ) { }
+    private toastr: ToastrService,
+    public fb: FormBuilder
+  ) {
+    this.definirFormulario();
+  }
 
   ngOnInit(): void {
     this.titleService.setTitle("GMA Sistema - Reestablecer contraseña");
   }
 
+  definirFormulario() {
+      this.formulario = this.fb.group({
+        correo: ["", [Validators.required, Validators.email]]
+      });
+    }
+
+    errorEnControlador (controlador: string, error: string) {
+      return (
+        this.formulario.controls[controlador].hasError(error) && 
+        this.formulario.controls[controlador].invalid &&
+        (this.formulario.controls[controlador].touched)
+      );
+    }
+
   reestablecerContrasenna () {
-    if (this.correo !== "") {
+    this.formulario.markAllAsTouched();
+    if (this.formulario.valid) {
+      this.correo = this.formulario.controls["correo"].value;
       this.usuarioService.reestablecerContrasena(this.correo).then(respuesta => {
         this.toastr.success("Correo enviado", "Reestablecer contraseña", {
           closeButton: true,
