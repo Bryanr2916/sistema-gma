6103585713ed22c0a28738a1829517e5b06a0ba2
Bryan Rodríguez <bryanr2916@gmail.com>
2025-08-01 17:30:55 -0600

    matrices: bloquear seleccion de empresas por tipo usuario

--- src/app/matrices/create/create.component.ts
+++ src/app/matrices/create/create.component.ts
@@ -6,6 +6,7 @@ import { ToastrService } from 'ngx-toastr';
 import { EmpresasService } from 'src/app/core/services/empresas.service';
 import { MatricesService } from 'src/app/core/services/matrices.service';
 import { MensajesService } from 'src/app/core/services/mensajes.service';
+import { UsuarioService } from 'src/app/core/services/usuario.service';
 import { seleccionVacia } from 'src/app/core/validators/seleccion-vacia';
 
 @Component({
@@ -14,7 +15,7 @@ import { seleccionVacia } from 'src/app/core/validators/seleccion-vacia';
   styleUrls: ['./create.component.scss']
 })
 export class CreateComponent implements OnInit {
-
+  cargando = true;
   formulario: FormGroup = this.fb.group({});
   matriz = {
     empresa: "",
@@ -23,12 +24,15 @@ export class CreateComponent implements OnInit {
 
   empresas: any[] = [];
 
+  usuario: any = {};
+
   constructor(
     private titleService: Title,
     private empresaService: EmpresasService,
     private matricesService: MatricesService,
     private mensajesService: MensajesService,
-    private router: Router, public fb: FormBuilder
+    private router: Router, public fb: FormBuilder,
+    private usuarioService: UsuarioService
     ) {
       this.definirFormulario();
     }
@@ -42,8 +46,29 @@ export class CreateComponent implements OnInit {
 
   ngOnInit(): void {
     this.titleService.setTitle("GMA Sistema - Matrices");
-    this.empresaService.obtenerEmpresas().subscribe(datos => {
-      this.empresas = datos;
+    this.usuarioService.usuarioActual().subscribe( usuarioActivo => {
+      if (usuarioActivo) { 
+        this.usuarioService.usuarioActualFS(usuarioActivo.uid).then(respuesta => {
+          const usuarioFS = respuesta.docs[0];
+          if (usuarioFS) {
+            const usuarioUID = respuesta.docs[0].data();
+            if (usuarioUID) {
+              this.usuario = usuarioUID
+              this.empresaService.obtenerEmpresas().subscribe(datos => {
+                this.empresas = datos;
+                if (this.usuario.tipo === 2) {
+                  this.formulario.get("empresa")?.disable();
+                  const empresa = this.empresas.find(emp => emp.id === this.usuario.empresaId);
+                  this.formulario.get("empresa")?.setValue(empresa.id);
+                }
+                this.cargando = false;
+              });
+            }
+          }
+        });
+      } else {
+        this.usuario.correo = "";
+      }
     });
   }
 
--- src/app/matrices/edit/edit.component.ts
+++ src/app/matrices/edit/edit.component.ts
@@ -6,6 +6,7 @@ import { ToastrService } from 'ngx-toastr';
 import { EmpresasService } from 'src/app/core/services/empresas.service';
 import { MatricesService } from 'src/app/core/services/matrices.service';
 import { MensajesService } from 'src/app/core/services/mensajes.service';
+import { UsuarioService } from 'src/app/core/services/usuario.service';
 import { seleccionVacia } from 'src/app/core/validators/seleccion-vacia';
 
 @Component({
@@ -14,7 +15,7 @@ import { seleccionVacia } from 'src/app/core/validators/seleccion-vacia';
   styleUrls: ['./edit.component.scss']
 })
 export class EditComponent implements OnInit {
-
+  cargando = true;
   formulario: FormGroup = this.fb.group({});
   matriz = {
     empresa: "",
@@ -24,6 +25,8 @@ export class EditComponent implements OnInit {
 
   empresas: any[] = [];
 
+  usuario: any = {};
+
   constructor(
     private titleService: Title,
     private empresaService: EmpresasService,
@@ -31,7 +34,8 @@ export class EditComponent implements OnInit {
     private mensajesService: MensajesService,
     private router: Router,
     private route: ActivatedRoute,
-    public fb: FormBuilder
+    public fb: FormBuilder,
+    private usuarioService: UsuarioService
   ) {
     this.definirFormulario();
   }
@@ -48,14 +52,36 @@ export class EditComponent implements OnInit {
     this.route.params.subscribe( params => {
       this.matriz.id = params["id"];
     });
-    this.matricesService.obtenerMatriz(this.matriz.id).then(respuesta => {
-      this.formulario.controls["titulo"].setValue(respuesta.get("titulo"));
-      this.formulario.controls["empresa"].setValue(respuesta.get("empresa"));
-      this.matriz.titulo = respuesta.get("titulo");
-      this.matriz.empresa = respuesta.get("empresa");
-    });
-    this.empresaService.obtenerEmpresas().subscribe(datos => {
-      this.empresas = datos;
+
+    this.usuarioService.usuarioActual().subscribe( usuarioActivo => {
+      if (usuarioActivo) { 
+        this.usuarioService.usuarioActualFS(usuarioActivo.uid).then(respuesta => {
+          const usuarioFS = respuesta.docs[0];
+          if (usuarioFS) {
+            const usuarioUID = respuesta.docs[0].data();
+            if (usuarioUID) {
+              this.usuario = usuarioUID
+              this.empresaService.obtenerEmpresas().subscribe(datos => {
+                this.empresas = datos;
+                if (this.usuario.tipo === 2) {
+                  this.formulario.get("empresa")?.disable();
+                } 
+
+                this.matricesService.obtenerMatriz(this.matriz.id).then(respuesta => {
+                  this.formulario.controls["titulo"].setValue(respuesta.get("titulo"));
+                  this.formulario.controls["empresa"].setValue(respuesta.get("empresa"));
+                  this.matriz.titulo = respuesta.get("titulo");
+                  this.matriz.empresa = respuesta.get("empresa");
+                });
+
+                this.cargando = false;
+              });
+            }
+          }
+        });
+      } else {
+        this.usuario.correo = "";
+      }
     });
   }
 
